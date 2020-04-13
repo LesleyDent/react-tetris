b0af9a9e2fcc3f539c2af2617b367026bfc6a69b
Lesley Dent <lesleyjdent@gmail.com>
2020-04-13 15:07:01 -0400

    added custom hooks
    usePlayer and useStage initial functions to draw tetrominos.
    tetris.js initial movement functions

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,24 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { createStage } from '../gameHelpers';
+
+// Styled Components
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+
+// CustomHooks
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
+
+// Components
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
-import { createStage } from '../gameHelpers';
-import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [stage, setStage] = useStage(player, resetPlayer);
+
+  console.log('re-render');
+
+  const movePlayer = dir => {
+    updatePlayerPos({ x: dir, y: 0 });
+  }
+
+  const startGame = () => {
+    // reset everything
+    setStage(createStage());
+    resetPlayer();
+  }
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false })
+  }
+
+  const dropPlayer = () => {
+    drop();
+  }
+  // destructuring keyCode here so don't have to event.keyCode
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        // left
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        // right
+        movePlayer(1)
+      } else if (keyCode === 40) {
+        // down
+        dropPlayer();
+      }
+    }
+  }
 
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role='button' tabIndex='0' onKeyDown={e => move(e)}>
       <StyledTetris>
-        <Stage stage={createStage()} />
+        <Stage stage={stage} />
         <aside>
-          <div>
-            <Display text="Score" />
-            <Display text="Rows" />
-            <Display text="Level" />
-          </div>
-          <StartButton />
+          {gameOver ? (
+            <Display gameOver={gameOver} text='Game Over' />
+          ) : (
+              <div>
+                <Display text="Score" />
+                <Display text="Rows" />
+                <Display text="Level" />
+              </div>
+            )}
+          <StartButton onClick={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
