f8fdbd2def1f0504801a80546eae10d042e191ee
Lesley Dent <lesleyjdent@gmail.com>
2020-07-08 16:44:30 -0400

    update music autoplay

--- src/components/MusicButton.js
+++ src/components/MusicButton.js
@@ -8,7 +8,7 @@ const MusicButton = () => (
   <div>
     {/* this.myRef = React.createRef(); */}
     {/* <StyledButton>||   Music</StyledButton> */}
-    <audio className='audio' src={Music} controls autoPlay loop />
+    <audio className='audio' src={Music} controls loop />
     {/* <iframe title='music' width="100%" height="166" scrolling="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/282742892&color=%2319ccf3&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe> */}
   </div>
 );
